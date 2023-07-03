b3790b1c41678d833807b28727075a2ffcf35d99
AjayMalviya <ajay.malpahwizz@gmail.com>
2023-07-03 18:00:56 +0100

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,22 @@
+final String appName = 'URC';
+
+final String packageName = 'com.urc.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.urc.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = '123456789';
+
+final String deepLinkUrlPrefix = 'https://eshopmultivendor.page.link';
+final String deepLinkName = 'URC Serves The Fresh';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+//final String baseUrl = 'https://vendor.eshopweb.store/app/v1/api/';
+// final String baseUrl = 'https://alphawizztest.tk/Food On The Way_Ecom/app/v1/api/';
+// final String baseUrl = 'https://foodontheways.com/app/v1/api/';
+final String baseUrl = 'https://developmentalphawizz.com/URC/app/v1/api/';
+
+final String imageUrl = 'https://developmentalphawizz.com/';
+final String jwtKey = "0352e7a815f965e8e278d47976b8f9cf466e1f42";
